b5406f216c3ba5657c02c34ed594877f8328ae65
PapaKramb <guccigang5554@gmail.com>
2022-02-08 18:44:47 +0300

    Refactoring posts controller

--- Rails_Uchi_Blog/app/controllers/posts_controller.rb
+++ Rails_Uchi_Blog/app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: %i[show edit update destroy]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show;end
 
   def new
     @post = Post.new
@@ -21,13 +21,9 @@ class PostsController < ApplicationController
     end
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit;end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       redirect_to @post
     else
@@ -36,8 +32,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
-
     if @post.destroy
       redirect_to posts_path
     end
@@ -47,5 +41,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
